740a4f5450ed00ef0df0f199a3fbb986760b59e0
Yash Mehan <yash.m@research.iiit.ac.in>
2021-05-13 20:02:15 +0530

    completed pair.h

---
+++ utils/pair.h
@@ -0,0 +1,19 @@
+//#pragma once
+#ifndef __PAIR_H
+#define __PAIR_H
+
+struct pair
+{
+    int first;
+    int second;
+};
+typedef struct pair pair;
+
+struct pairForQ
+{
+    double first;
+    int second;
+};
+typedef struct pairForQ pairForQ;
+
+#endif
